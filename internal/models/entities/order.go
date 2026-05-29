d0fad60a85411a5f8343d98c4b3452620e3f8cbc
le.vinh.lam <le.vinh.lam@sun-asterisk.com>
2026-05-29 17:46:52 +0700

    optimize code

--- internal/models/entities/order.go
+++ internal/models/entities/order.go
@@ -7,6 +7,6 @@ type Order struct {
 	UserID     uint         `json:"user_id"`
 	User       User         `gorm:"foreignKey:UserID" json:"-"`
 	TotalPrice float64      `gorm:"type:decimal(10,2);not null" json:"total_price"`
-	Status     string       `gorm:"type:varchar(50);default:'pending'" json:"status"` // pending, processing, completed, cancelled
+	Status     string       `gorm:"type:varchar(50);default:'pending'" json:"status"` // pending, processing, delivering, completed, cancelled
 	OrderItems []*OrderItem `gorm:"foreignKey:OrderID" json:"order_items"`
 }
